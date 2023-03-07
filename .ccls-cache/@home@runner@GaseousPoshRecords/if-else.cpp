2b578ec58722222e7df2727a29a0256955f6471f
Nisha Sikarwar <104161288+nishu311@users.noreply.github.com>
2023-03-07 05:00:20 +0000

    if-else check code

---
+++ .ccls-cache/@home@runner@GaseousPoshRecords/if-else.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+cin>>a;
+if(a>0){
+  cout<<"a is the postive number ";
+  
+}
+else{
+  cout<<"a is the nagative number";
+  }
+else{
+  cout<<'a is 0';
+  }
+}
